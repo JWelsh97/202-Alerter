5b7d0f918f6598732c9f3a20f96a10209977e2c3
JWelsh97 <jackwelsh@protonmail.com>
2015-10-29 03:40:34 +0000

    Added the get_devices function

--- pushbullet.py
+++ pushbullet.py
@@ -1,14 +1,35 @@
 import requests
 import json
 
+class Pushbullet(object):
+    def __init__(self):
+        self.__s = requests.session()
+        self.__s.headers = {"Access Token": access_token}
+    
 
-def get_devices()
+    def get_devices(access_token):
+        """
+        Gathers pushbullet directory
+        of all devices
+        """
+        try:
+            r = json.loads(self.__s.get("https://api.pushbullet.com/v2/devices").text)
+        except:    
+            result = []
+        if "devices" in request:
+            devices = request["devices"]
+            result = []
+            for device in devices:
+                if device["active"]:
+                    result.append(device)
+        return result
 
+    def pushable(devices):
+        """
+        Takes pushbullet directory and
+        checks if they are pushable
+        """
+        
+        
 
-def phones(devices)
-
-
-def pushable(devices)
-
-
-def push_note(device_iden, title, body)
+    def push_note(device_iden, title, body):
