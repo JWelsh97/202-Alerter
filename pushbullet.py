dfc44102a402d9a5dcbcc8a1e6ad5e3045e52310
JWelsh97 <jackwelsh@protonmail.com>
2015-10-29 08:37:04 +0000

    Fixed some names

--- pushbullet.py
+++ pushbullet.py
@@ -1,19 +1,16 @@
 import requests
 import json
 
+
 class PushBullet(object):
     def __init__(self, access_token):
         self.__s = requests.session()
         self.__s.headers = {"Access-Token": access_token}
-    
+
     def get_devices(self):
-        """
-        Gathers pushbullet directory
-        of all devices
-        """
         try:
             r = json.loads(self.__s.get("https://api.pushbullet.com/v2/devices").text)
-        except:    
+        except:
             result = []
         if "devices" in r:
             devices = r["devices"]
@@ -22,16 +19,16 @@ class PushBullet(object):
                 if device["active"]:
                     result.append((device["nickname"], device["iden"]))
         return result
-        
-    def push_note(self, title, body, devices):
-        result = []
-        if isinstance(devices, list):
-            for iden in devices:
-                post = self.__s.post("https://api.pushbullet.com/v2/pushes",
-                                     data={"type": "note",
-                                           "device_iden": iden,
-                                           "title": title,
-                                           "body": body})
-                result.append(post.text)
-        return result
 
+def push_note(self, title, body, devices):
+    result = []
+    if isinstance(devices, list):
+        for iden in devices:
+            post = self.__s.post("https://api.pushbullet.com/v2/pushes",
+                                 data={"type": "note",
+                                       "device_iden": iden,
+                                       "title": title,
+                                       "body": body})
+            result.append(post.text)
+    return result
+                
