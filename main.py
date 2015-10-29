27f240bf17d46ae0b573e23d94a8c3e3c3d6ad30
JWelsh97 <jackwelsh@protonmail.com>
2015-10-29 05:24:47 +0000

    Added the arg --list

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
-
+import sys
+import yaml
+from pushbullet import PushBullet
 
 def sitestatus(url):
-    '''
+    """
     Determines why the site
     cannot be accessed, places
     data in tuple
-    '''
+    """
     try:
         r = requests.get(url)
     except requests.ConnectionError:
@@ -16,11 +18,24 @@ def sitestatus(url):
     except Exception as e:
         return ("Failure", str(e))
 
+
 def read_config():
-    '''
+    """
     Reads the YAML config file
-    '''
+    """
     f = open("config.yaml", "r")
     conf = f.read()
     f.close()
     return yaml.load(conf)
+
+
+conf = read_config()
+sitestatus = sitestatus(conf["site"])
+print sitestatus
+pb = PushBullet(conf["access_token"])
+if sys.argv > 1:
+    for arg in sys.argv:
+        if sys.arg == "--list":
+            for device in pb.get_devices():
+                print device
+
