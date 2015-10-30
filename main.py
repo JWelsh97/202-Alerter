b573f4b96ecfc748ac5b0a0978557a8e13e51ad8
JWelsh97 <jackwelsh@protonmail.com>
2015-10-30 01:02:20 +0000

    Added SiteStatus class, placed main into function

--- main.py
+++ main.py
@@ -3,52 +3,75 @@ import sys
 import yaml
 from pushbullet import PushBullet
 import datetime
+from enum import Enum
 
 
-def sitestatus(url):
+class SiteStatus(Enum):
+    error = 1
+    down = 2
+    up = 3
+
+
+def site_status(url):
     """
     Determines why the site
     cannot be accessed, places
     data in tuple
     """
     try:
-        r = requests.get(url)
+        r = requests.get(url, timeout=5)
     except requests.ConnectionError:
-        return ("Error!", "Webserver is offline")
+        return {"state": SiteStatus.error,
+                "status": -1,
+                "reason": "Webserver is offline"}
     except requests.Timeout:
-        return ("Error!", "Server is offline")
+        return {"state": SiteStatus.error,
+                "status": -1,
+                "reason":  "Server is offline"}
     except Exception as e:
-        return ("Error!", str(e))
-    else:
-        return ("Up!", "Site is up!")
+        return {"state": SiteStatus.error,
+                "status": -1,
+                "reason": str(e)}
 
+    if r.status_code == "200":
+        return {"state": SiteStatus.up,
+                "status": r.status_code,
+                "reason": r.reason}
+    else:
+        return {"state": SiteStatus.up,
+                "status": r.status_code,
+                "reason": r.reason}
 
 
 def read_config():
     """
     Reads the YAML config file
     """
-    f = open("config.yaml", "r")
-    conf = f.read()
-    f.close()    
-    return yaml.load(conf)
+    with open("config.yaml", "r") as f:
+        conf = yaml.load(f)
+    return conf
 
 
-if sitestatus("https://google.com")[0] == "Up!":
-    dateandtime = datetime.datetime.now().strftime("%H:%M:%S %d/%m/%y")
-    conf = read_config()
-    sitestatus = sitestatus(conf["site"])
-    pb = PushBullet(conf["access_token"])
-    get_devices = pb.get_devices()
-    index = -1
-    if sys.argv > 1:
-        for arg in sys.argv:
-            if arg == "--list":
-                arg = True
-                for device in get_devices:
-                    index += 1
-                    print "%s: %s" % (get_devices[index][0], get_devices[index][1])
-            else:
-                arg = False
-    if arg == False and sitestatus[0] == "Error!":
-        pb.push_note(sitestatus[0], "%s as of %s" % (sitestatus[1], dateandtime), conf["devices"])
+def main(pb, conf):
+    status = site_status(conf["site"])
+    dt_time = datetime.datetime.now().strftime("%I:%M%p %d/%m/%y")
+    devices = conf["devices"]
+
+    if status["state"] == SiteStatus.down:
+        pb.push_note("Website Unavailable",
+                     "%s as of %s" % (status["reason"], dt_time),
+                     devices)
+    elif status["state"] == SiteStatus.error:
+        pb.push_note("Website Offline",
+                     "%s as of %s" % (status["reason"], dt_time),
+                     devices)
+
+
+conf = read_config()
+pb = PushBullet(conf["access_token"])
+
+if "--list" in sys.argv:
+    for device in pb.get_devices():
+        print("%s: %s" % device)
+else:
+    main(pb, conf)
